36a40ce318d6e92aa31d0c80072205bdc19b60f3
InvsbleBeat87 <117841703+InvsbleBeat87@users.noreply.github.com>
2022-11-14 19:55:24 -0600

    animated player character

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,11 +7,14 @@ public class PlayerBehavior : MonoBehaviour
     public Vector2 jumpForce = new Vector2(0, 1250);
 
     private Rigidbody2D rb2D;
+    private Animator anim;
+    private bool isWalking;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public float speed;
@@ -33,6 +36,17 @@ public class PlayerBehavior : MonoBehaviour
         Vector3 playerPos = transform.position;
         playerPos.x += xMove * Time.deltaTime * speed;
         transform.position = playerPos;
+        
+        if(xMove == 0)
+        {
+            isWalking = false;
+            anim.SetTrigger("goToIdle");
+        }
+        else if(isWalking != true)
+        {
+            isWalking = true;
+            anim.SetTrigger("goToWalkCycle");
+        }
         
         if(Timer > 0)
         {
